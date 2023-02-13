1db5bb31de6e6e123746885b6b8633cad1fe6368
paulsalazar24 <70818490+paulsalazar24@users.noreply.github.com>
2023-02-13 14:07:10 -0500

    subir cambios

---
+++ Python/tipos de datos/tipos-de-datos.py
@@ -0,0 +1,14 @@
+# cade de texto
+"string"
+'strig'
+
+
+
+# nos pemite hacer saltos de linea  
+"""string:
+    nombre: paul
+"""
+'''hola'''
+
+4
+4.4
---
+++ Python/variables/variables.py
@@ -0,0 +1,22 @@
+a = 2
+b = 3
+c = a + b
+
+print(c)
+
+nombre = "paul salazar"
+print(nombre)
+
+
+nombreL = "lol"
+nombreL = "pol"
+nombreL = "lex"
+
+numero = 10
+numero += 1
+
+print(numero)
+
+ 
+ 
+ 
